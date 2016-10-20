f572c78f2a531e54567cd33bdb31454ee90eb714
ryanoverhauser <ryan.overhauser@gmail.com>
2016-10-20 11:28:16 -0700

    Formatting

--- server/modules/database.js
+++ server/modules/database.js
@@ -8,7 +8,17 @@ function Database() {
   return {
 
     getDecks: function(callback) {
-      connection.query('SELECT id, name, (SELECT COUNT(white_cards.id) FROM white_cards WHERE white_cards.deck_id = card_decks.id) as white_card_count, (SELECT COUNT(black_cards.id) FROM black_cards WHERE black_cards.deck_id = card_decks.id) as black_card_count FROM card_decks', function(err, rows, fields) {
+      var queryString = 'SELECT id, name, \
+        ( SELECT COUNT(white_cards.id) \
+          FROM white_cards \
+          WHERE white_cards.deck_id = card_decks.id \
+        ) as white_card_count, \
+        ( SELECT COUNT(black_cards.id) \
+          FROM black_cards \
+          WHERE black_cards.deck_id = card_decks.id \
+        ) as black_card_count \
+        FROM card_decks';
+      connection.query(queryString, function(err, rows, fields) {
         if (err) throw err;
         callback(rows);
       });
@@ -16,10 +26,12 @@ function Database() {
     getAllCards: function(callback){
       var white,
           black;
-      connection.query('SELECT id, text FROM white_cards', function(err, rows, fields) {
+      var queryString = 'SELECT id, text FROM white_cards';
+      connection.query(queryString, function(err, rows, fields) {
         if (err) throw err;
         white = rows;
-        connection.query('SELECT id, text, draw, pick FROM black_cards', function(err, rows, fields) {
+        queryString = 'SELECT id, text, draw, pick FROM black_cards';
+        connection.query(queryString, function(err, rows, fields) {
           if (err) throw err;
           black = rows;
           callback(white, black);
@@ -29,10 +41,23 @@ function Database() {
     getActiveCards: function(callback){
       var white,
           black;
-      connection.query('SELECT white_cards.id, white_cards.text FROM white_cards INNER JOIN card_decks ON white_cards.deck_id = card_decks.id WHERE white_cards.active = 1 AND card_decks.active = 1', function(err, rows, fields) {
+      var queryString = 'SELECT white_cards.id, white_cards.text \
+        FROM white_cards \
+        INNER JOIN card_decks \
+        ON white_cards.deck_id = card_decks.id \
+        WHERE white_cards.active = 1 \
+        AND card_decks.active = 1';
+      connection.query(queryString, function(err, rows, fields) {
         if (err) throw err;
         white = rows;
-        connection.query('SELECT black_cards.id, black_cards.text, black_cards.draw, black_cards.pick FROM black_cards INNER JOIN card_decks ON black_cards.deck_id = card_decks.id WHERE black_cards.active = 1 AND card_decks.active = 1', function(err, rows, fields) {
+        queryString = 'SELECT black_cards.id, black_cards.text, \
+          black_cards.draw, black_cards.pick \
+          FROM black_cards \
+          INNER JOIN card_decks \
+          ON black_cards.deck_id = card_decks.id \
+          WHERE black_cards.active = 1 \
+          AND card_decks.active = 1';
+        connection.query(queryString, function(err, rows, fields) {
           if (err) throw err;
           black = rows;
           callback(white, black);
@@ -47,10 +72,18 @@ function Database() {
         if (i > 0) ors = ors + ' OR ';
         ors = ors + 'deck_id = ' + decks[i];
       }
-      connection.query('SELECT id, text FROM white_cards WHERE active = 1 AND (' + ors + ')', function(err, rows, fields) {
+      var queryString = 'SELECT id, text \
+        FROM white_cards \
+        WHERE active = 1 \
+        AND (' + ors + ')';
+      connection.query(queryString, function(err, rows, fields) {
         if (err) throw err;
         white = rows;
-        connection.query('SELECT id, text, draw, pick FROM black_cards WHERE active = 1 AND (' + ors + ')', function(err, rows, fields) {
+        queryString = 'SELECT id, text, draw, pick \
+          FROM black_cards \
+          WHERE active = 1 \
+          AND (' + ors + ')';
+        connection.query(queryString, function(err, rows, fields) {
           if (err) throw err;
           black = rows;
           callback(white, black);
