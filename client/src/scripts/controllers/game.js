ac540094c912a9c90c0458caaead5fba88273631
Ryan Overhauser <ryan.overhauser@gmail.com>
2016-10-25 23:58:58 -0700

    Make hand cards draggable to play slots, style tweaks

--- client/src/scripts/controllers/game.js
+++ client/src/scripts/controllers/game.js
@@ -13,20 +13,43 @@
 
     $ctrl.hand = {};
     $ctrl.round = {};
+    $ctrl.placeCard = placeCard;
+    $ctrl.playSlots = [];
 
     socket.on('joinedGame', onJoinedGame);
     socket.on('hand', onHand);
     socket.on('newRound', onNewRound);
     socket.on('roundStatus', onRoundStatus);
 
+    function setupRound() {
+      for (var i = 0; i < $ctrl.round.prompt.pick; i++) {
+        $ctrl.playSlots.push({
+          id: i,
+          card: false
+        });
+      }
+    }
+
+    function placeCard(slot, card) {
+      if (!card.disabled) {
+        if (slot.card) {
+          slot.card.disabled = false;
+        }
+        slot.card = card;
+        card.disabled = true;
+      }
+    }
+
     function onJoinedGame(data) {
       console.log('joinedGame', data);
       $ctrl.round = data.round;
+      setupRound();
     }
 
     function onNewRound(data) {
       console.log('newRound', data);
       $ctrl.round = data;
+      setupRound();
     }
 
     function onRoundStatus(data) {
