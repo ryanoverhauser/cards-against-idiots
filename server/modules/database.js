c4feea53d2b56d61f4a95c19b3005d37d6be3bd1
Ryan Overhauser <ryan.overhauser@gmail.com>
2016-10-27 14:16:37 -0700

    Close database connections after query

--- server/modules/database.js
+++ server/modules/database.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var debug = require('debug')('database'),
-    mysql = require('mysql');
+var debug = require('debug')('database');
+var mysql = require('mysql');
 
 function BlackCard(id, text, draw, pick) {
   this.id = id;
@@ -22,47 +22,6 @@ function WhiteCard(id, text) {
 }
 
 function Database() {
-
-  var connection = mysql.createConnection({
-    host        : process.env.DB_HOST,
-    port        : process.env.DB_PORT,
-    user        : process.env.DB_USER,
-    password    : process.env.DB_PASSWORD,
-    database    : process.env.DB_NAME
-  });
-
-  connection.connect(function(err) {
-    if (err) { throw err };
-    debug('Successfuly opened MySql connection');
-  });
-
-  function runQuery(queryString) {
-    return new Promise((resolve, reject) => {
-      connection.query(queryString, function(err, rows, fields) {
-        if (err) { reject(err) };
-        debug('Query complete');
-        resolve(rows);
-      });
-    });
-  }
-
-  function parseWhiteCards(rows) {
-    var cards = [];
-    for (let r of rows) {
-      var card = new WhiteCard(r.id, r.text);
-      cards.push(card);
-    }
-    return cards;
-  }
-
-  function parseBlackCards(rows) {
-    var cards = [];
-    for (let r of rows) {
-      var card = new BlackCard(r.id, r.text, r.draw, r.pick);
-      cards.push(card);
-    }
-    return cards;
-  }
 
   function getDecks() {
     var queryString = 'SELECT id, name, ' +
@@ -147,11 +106,57 @@ function Database() {
     });
   }
 
+  function newConnection() {
+    var connection =  mysql.createConnection({
+      host        : process.env.DB_HOST,
+      port        : process.env.DB_PORT,
+      user        : process.env.DB_USER,
+      password    : process.env.DB_PASSWORD,
+      database    : process.env.DB_NAME
+    });
+
+    connection.connect(function(err) {
+      if (err) { throw err };
+    });
+
+    return connection;
+  }
+
+  function parseBlackCards(rows) {
+    var cards = [];
+    for (let r of rows) {
+      var card = new BlackCard(r.id, r.text, r.draw, r.pick);
+      cards.push(card);
+    }
+    return cards;
+  }
+
+  function parseWhiteCards(rows) {
+    var cards = [];
+    for (let r of rows) {
+      var card = new WhiteCard(r.id, r.text);
+      cards.push(card);
+    }
+    return cards;
+  }
+
+  function runQuery(queryString) {
+    return new Promise((resolve, reject) => {
+      var connection = newConnection();
+      connection.query(queryString, function(err, rows, fields) {
+        if (err) { reject(err) };
+        connection.end();
+        debug('Query complete');
+        resolve(rows);
+      });
+    });
+  }
+
   return {
-    getDecks: getDecks,
-    getAllCards: getAllCards,
     getActiveCards: getActiveCards,
-    getCardsFromDecks: getCardsFromDecks
+    getAllCards: getAllCards,
+    getCardsFromDecks: getCardsFromDecks,
+    getDecks: getDecks
   }
 
 }
